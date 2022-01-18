1b877e49ed213b0e1178fe0a8bc600648dccdcc5
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2022-01-18 18:56:01 -0300

    Internal server errors now gets logged

--- server/src/routes/balance/balance.controller.js
+++ server/src/routes/balance/balance.controller.js
@@ -5,6 +5,7 @@ async function httpGetBalance(req, res) {
         const balance = await getBalance();
         return res.status(200).json(balance);
     } catch (err) {
+        console.error(err);
         res.status(500).json({
             error: 'Internal server error',
         });
--- server/src/routes/types/types.controller.js
+++ server/src/routes/types/types.controller.js
@@ -5,6 +5,7 @@ async function httpGetAllTypes(req, res) {
         const types = await getAllTypes();
         return res.status(200).json(types);
     } catch (err) {
+        console.error(err);
         res.status(500).json({
             error: 'Internal server error',
         });
